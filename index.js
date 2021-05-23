7ed4b3432778e8096f9252e6b3162e026b21be08
Mikołaj Gramowski <emeraldbob020@gmail.com>
2021-05-23 11:54:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import express from 'express';
+import bodyParser from 'BodyParser';
+import mongoose from 'mongoose';
+import cors from 'cors';
---
+++ readme.md
@@ -0,0 +1,6 @@
+# React side
+npx create-react-app ./
+
+# Server side
+npm init -y
+npm install body-parser cors express mongoose
